a0bf2d75e2088542fb3fd8b36f214f0a0dd7cb1e
Matthew Fudge <mfudg395@mtroyal.ca>
2021-02-18 15:59:01 -0700

    added fetch and creation of stockDataTable

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const companiesAPI = "https://www.randyconnolly.com/funwebdev/3rd/api/stocks/companies.php";
+const stockDataAPI = "https://www.randyconnolly.com/funwebdev/3rd/api/stocks/history.php?symbol=";
 
+const currency = new Intl.NumberFormat('en-us', {style: 'currency', currency: 'USD'}); // used for formatting dollar values
 let map;
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -9,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const viewButtons = document.querySelectorAll(".changeViewButton");
 
     let htmlCompanyList = document.querySelector("#companyList"); // Node for the unordered list of companies
+    let stockTable = document.querySelector("#stockDataTable"); // Node for the table containing stock information
 
     // Adding event listeners for the button that switches between views.
     for (let button of viewButtons) {
@@ -81,6 +84,7 @@ document.addEventListener('DOMContentLoaded', function() {
             element.textContent = company.name;
             element.addEventListener('click', function() {
                 setMap(company);
+                setStockData(company);
             });
             htmlCompanyList.append(element);
         }
@@ -92,6 +96,48 @@ document.addEventListener('DOMContentLoaded', function() {
             center: {lat: company.latitude, lng: company.longitude},
             zoom: 18
         });
+    }
+
+    // Builds the Stock Data panel, displaying a table of 3 months of stock data from the given company.
+    async function setStockData(company) {
+        const tableLoadingAnimation = document.querySelector('#stockDataLoadingAnimation');
+        const tableBody = stockTable.querySelector('tbody');
+        tableBody.innerHTML = ""; // clear all current data in table
+
+        tableLoadingAnimation.style.display = 'block'; // display the loading animation during fetch
+        const response = await fetch(stockDataAPI + company.symbol);
+        const stockData = await response.json();
+        tableLoadingAnimation.style.display = 'none'; // hide loading animation once fetch complete
+
+        for (let data of stockData) { // each piece of 'data' will be a single row in the table
+            let row = document.createElement('tr');
+
+            let dateCell = document.createElement('td');
+            dateCell.textContent = data.date;
+            row.appendChild(dateCell);
+
+            let openCell = document.createElement('td');
+            openCell.textContent = currency.format(data.open);
+            row.appendChild(openCell);
+
+            let closeCell = document.createElement('td');
+            closeCell.textContent = currency.format(data.close);
+            row.appendChild(closeCell);
+
+            let lowCell = document.createElement('td');
+            lowCell.textContent = currency.format(data.low);
+            row.appendChild(lowCell);
+
+            let highCell = document.createElement('td');
+            highCell.textContent = currency.format(data.high);
+            row.appendChild(highCell);
+
+            let volumeCell = document.createElement('td');
+            volumeCell.textContent = currency.format(data.volume);
+            row.appendChild(volumeCell);
+
+            tableBody.appendChild(row);
+        }
     }
 
     /* Creates event listener for the Filter search bar in the Company List panel. When the text
