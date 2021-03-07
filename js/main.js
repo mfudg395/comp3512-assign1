e9c4ddd7e9bf9832817880d3e0f10f5d4901a05f
Matthew Fudge <mfudg395@mtroyal.ca>
2021-03-07 01:51:14 -0700

    fixed filter search, added handling for null financials

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ const currency = (num) => { return new Intl.NumberFormat('en-us', {style: 'curre
 /* converts a currency to a number, to be used when sorting table data 
 *  adopted from https://stackoverflow.com/questions/31197542/javascript-sort-for-currency-string*/
 const asNumber = (currency) => {return Number(currency.replace(/($,)/g,''));} 
+const defaultFinancialHTML = document.querySelector('#financials').innerHTML;
+const defaultChartHTML = document.querySelector('#charts').innerHTML;
 let map;
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -14,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const chartViewElements = document.querySelectorAll(".chartView");
     const viewButtons = document.querySelectorAll(".changeViewButton");
 
-    let htmlCompanyList = document.querySelector("#companyList"); // Node for the unordered list of companies
+    let htmlCompanyList = document.querySelector("#companyList"); // Node for the unordered list of companies.
 
     // Displays the assignment credits for five seconds after mousing over the pencil icon.
     document.querySelector('.credits').addEventListener('mouseover', function() {
@@ -90,15 +92,22 @@ document.addEventListener('DOMContentLoaded', function() {
                 const stockData = await response.json();
                 tableLoadingAnimation.style.display = 'none';
 
+                if (company.financials == null) { // Some companies in the API are missing financial data. In those cases, the panels simply indicate as such.
+                    document.querySelector('#charts').innerHTML = "No financial data found.";
+                    document.querySelector('#financials').innerHTML = "No financial data found.";
+                } else { // If companies do have financial data, the affected HTML panels are reset to their default state so they can be modified properly.
+                    document.querySelector('#charts').innerHTML = defaultChartHTML;
+                    document.querySelector('#financials').innerHTML = defaultFinancialHTML;
+                }
+
                 // Building each panel based on the clicked company.
                 setCompanyInfo(company);
                 setMap(company);
                 setStockData(stockData);
 
-                setCharts(company, stockData);
                 setChartCompanyInfo(company);
+                setCharts(company, stockData);
                 setFinancials(company);
-                
             });
             htmlCompanyList.append(element); // appending the created element to the unordered list
         }
@@ -248,7 +257,6 @@ document.addEventListener('DOMContentLoaded', function() {
     function setBarChart(company) {
         const barChart = echarts.init(document.querySelector('#barChartContainer'));
         const chart = {
-            title: { text: `Financials` },
             tooltip: {},
             legend: { data: ['Revenue', 'Earnings', 'Assets', 'Liabilities'] },
             xAxis: { data: ["2019", "2018", "2017"] },
@@ -405,22 +413,22 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     /* Creates event listener for the Filter search bar in the Company List panel. When the text
-    *  field is populated, the list of companies filters to show names that match the search.
+    *  field is populated, the list of companies filters to show company symbols that match the search.
     */
     function createFilter(companies) {
         const searchFilter = document.querySelector('#companySearch');
-        searchFilter.addEventListener('input', function() {
+        searchFilter.addEventListener('change', function() {
             const matches = findMatches(searchFilter.value, companies);
             htmlCompanyList.innerHTML = "";
             setCompanyList(matches);
         });
     }
 
-    // Returns a filtered array of Company objects whose names start with the given 'value' parameter. 
+    // Returns a filtered array of Company objects whose symbols start with the given 'value' parameter. 
     function findMatches(value, companies) {
         return companies.filter(obj => {
             const regex = new RegExp(`^${value}`, 'i');
-            return obj.name.match(regex);
+            return obj.symbol.match(regex);
         });
     }
 });
